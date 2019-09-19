51175d1ee62fd4d2980118abcf23243ee6650bf5
Theodore Andrew <andrew.t@husky.neu.edu>
2019-09-19 01:56:57 -0700

    change something

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send({ hi: "there" });
-  console.log(req);
+  res.send({ hello: "world" });
 });
 
 // If we run in Heroku, it will use process.env.PORT
