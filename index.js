35e3664b691fda339705568a4251ec15d6501417
Theodore Andrew <andrew.t@husky.neu.edu>
2019-09-19 01:27:50 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send({ hi: "there" });
+  console.log(req);
+});
+
+// If we run in Heroku, it will use process.env.PORT
+// If we run it locally, it will be defaulted to port 5000
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
