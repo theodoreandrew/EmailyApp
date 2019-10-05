a6840dd0c54ddd548089db31b319f28b60e5d648
Theodore Andrew <andrew.t@husky.neu.edu>
2019-10-04 18:22:07 -0700

    modify root route

--- index.js
+++ index.js
@@ -22,6 +22,12 @@ app.use(passport.session());
 // This will execute function in authRoutes.js with app as the argument.
 require("./routes/authRoutes")(app);
 
+app.get("/", (req, res) => {
+  res.send({
+    Hello: "world"
+  });
+});
+
 // If we run in Heroku, it will use process.env.PORT
 // If we run it locally, it will be defaulted to port 5000
 const PORT = process.env.PORT || 5000;
